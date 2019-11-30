d2f9cee7e6a3dbc1e717b57daf2c8cf3f90b92cd
LauraJaneStevenson <laurajanestevenson628@gmail.com>
2019-11-30 04:55:00 +0000

    added new html files

---
+++ static/event_request_hover.js
@@ -0,0 +1,17 @@
+
+// $('.approved').on('mouseover',(evt) => {
+
+//     // get value for cal_id to pass through with ajax
+//     const button = $(evt.target);
+//     const id = button.attr('id');
+//     alert("notification ID:" + _id);
+//     // alert("hi");
+//     // console.log(cal_id)
+
+//     // make ajax request
+//     // $.post('/request_access_to_cal',{ 'cal_id': cal_id },(res) => {
+//     //     //alers user that request has been sent
+//     //     alert(res);
+//     // });
+
+// });
